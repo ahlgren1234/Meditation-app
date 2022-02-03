613fc99e6988d637f4f6e0383222b0960e37eef8
Peter Ahlgren <ahlgren1234@gmail.com>
2022-02-03 13:38:01 +0100

    Player View UI

---
+++ Meditation/PlayerView.swift
@@ -0,0 +1,120 @@
+//
+//  PlayerView.swift
+//  Meditation
+//
+//  Created by Peter Ahlgren on 2022-02-03.
+//
+
+import SwiftUI
+
+struct PlayerView: View {
+    
+    @State private var value: Double = 0.0
+    
+    var body: some View {
+        ZStack{
+            // MARK: Background Image
+            
+            Image("image-stones")
+                .resizable()
+                .scaledToFill()
+                .frame(width: UIScreen.main.bounds.width)
+                .ignoresSafeArea()
+            
+            // MARK: Blur View
+            
+            Rectangle()
+                .background(.thinMaterial)
+                .opacity(0.25)
+                .ignoresSafeArea()
+            
+            VStack(spacing: 32) {
+                // MARK: Dismiss Button
+                
+                HStack {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "xmark.circle.fill")
+                            .font(.system(size: 36))
+                            .foregroundColor(.white)
+                    }
+                    Spacer()
+                }
+                
+                // MARK: Title
+                
+                Text("1 Minute Relaxing Meditation")
+                    .font(.title)
+                    .foregroundColor(.white)
+                
+                Spacer()
+                
+                // MARK: Playback
+                
+                VStack(spacing: 5) {
+                    // MARK: Playback Timeline
+                    
+                    Slider(value: $value, in: 0...60)
+                        .accentColor(.white)
+                    
+                    // MARK: Playback Time
+                    
+                    HStack {
+                        Text("0:00")
+                        
+                        Spacer()
+                        
+                        Text("1:00")
+                    }
+                    .font(.caption)
+                    .foregroundColor(.white)
+                }
+                
+                // MARK: Playback Controls
+                
+                HStack {
+                    // MARK: Repeat Button
+                    PlaybackControlButton(systemName: "repeat") {
+                        
+                    }
+                    
+                    Spacer()
+                    
+                    // MARK: Backward Button
+                    PlaybackControlButton(systemName: "gobackward.10") {
+                        
+                    }
+                    
+                    Spacer()
+                    
+                    // MARK: Play/Pause Button
+                    PlaybackControlButton(systemName: "play.circle.fill", fontSize: 44) {
+                        
+                    }
+                    
+                    Spacer()
+                    
+                    // MARK: Forward Button
+                    PlaybackControlButton(systemName: "goforward.10") {
+                        
+                    }
+                    
+                    Spacer()
+                    
+                    // MARK: Stop Button
+                    PlaybackControlButton(systemName: "stop.fill") {
+                        
+                    }
+                }
+            }
+            .padding(20)
+        }
+    }
+}
+
+struct PlayerView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlayerView()
+    }
+}
