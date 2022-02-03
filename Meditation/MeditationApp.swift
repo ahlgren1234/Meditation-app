048f4fbd00791750355e2a79d4da0d03478ede88
Peter Ahlgren <ahlgren1234@gmail.com>
2022-02-03 12:41:42 +0100

    Initial Commit

---
+++ Meditation/MeditationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeditationApp.swift
+//  Meditation
+//
+//  Created by Peter Ahlgren on 2022-02-03.
+//
+
+import SwiftUI
+
+@main
+struct MeditationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
